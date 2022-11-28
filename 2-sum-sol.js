8cd301fb22aa0d2e14cd85f69ec94171069d735b
muhammadmohsin123 <mohsinsurani12@gmail.com>
2022-11-28 08:30:16 +0500

    brute force sol for max water container

--- 2-sum-sol.js
+++ 2-sum-sol.js
@@ -1,20 +1,40 @@
-const numsArray = [1, 3, 7, 9, 2];
-const targetToFind = 11;
+const numsArray = [1, 2, 3, 4];
+const targetToFind = 6;
 
 // Brute Force Solution
-const findTwoSum = function (nums, target) {
-  for (let p1 = 0; p1 < nums.length; p1++) {
-    const numberToFind = target - nums[p1];
+// const findTwoSum = function (nums, target) {
+//   for (let p1 = 0; p1 < nums.length; p1++) {
+//     const numberToFind = target - nums[p1];
 
-    for (let p2 = p1 + 1; p2 < nums.length; p2++) {
-      if (numberToFind === nums[p2]) {
-        return [p1, p2];
-      }
+//     for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+//       if (numberToFind === nums[p2]) {
+//         return [p1, p2];
+//       }
+//     }
+//   }
+
+//   return null;
+// };
+
+// console.log(findTwoSum(numsArray, targetToFind));
+
+//Using Hash Maps
+
+const findTwoSum = function (nums, target) {
+  const numsMap = {};
+
+  for (let p = 0; p < nums.length; p++) {
+    const currentMapVal = numsMap[nums[p]];
+
+    if (currentMapVal >= 0) {
+      return [currentMapVal, p];
+    } else {
+      const numberToFind = target - nums[p];
+      numsMap[numberToFind] = p;
     }
   }
 
   return null;
 };
 
-// console.log(findTwoSum(numsArray, targetToFind));
-const arr = [1, 3, 5, 7, 4];
+console.log(findTwoSum(numsArray, targetToFind));
